5d4f6eb8de05eb32e6e369e6af487edffda89c2b
danyeric123 <dnagarpowers@gmail.com>
2021-09-24 15:20:26 -0400

    solution to 15

--- my_solutions_ch17.py
+++ my_solutions_ch17.py
@@ -143,4 +143,11 @@ def find_closest(words,word1,word2):
 
 def smallest_k(nums : list[int], k: int)->list[int]:
   return nums.sort()[:k+1]
+
+def longestWord(words: list[str]) -> str:
+  valid = set([""])
+  for word in sorted(words, key=len):
+      if word[:-1] in valid:
+          valid.add(word)
+  return max(sorted(valid), key=len)
     
